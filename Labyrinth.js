d7a7c0cdbdf7789a1ce9d5e2e6e2686a91b4e95a
JaredM-Williams <43732284+JaredM-Williams@users.noreply.github.com>
2018-11-06 23:19:21 -0500

    Revert "WebsiteV1"

--- Labyrinth.js
+++
@@ -1,7 +0,0 @@
-for (var line=1; line<60; line++) {
-    for(var i=1;i<29;i++) {
-        var s = (Math.floor((Math.random()*2)%2)) ? "╱" : "╲";
-        document.write(s);
-    }
-    document.writeln("<br>");
-}
