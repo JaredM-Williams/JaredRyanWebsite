c0d7598d630e13651d3873fa7d4567b909b5030d
JaredM-Williams <43732284+JaredM-Williams@users.noreply.github.com>
2018-11-06 23:04:00 -0500

    Add files via upload

---
+++ JavaScriptTest.js
@@ -0,0 +1,25 @@
+var bool = confirm("shall we then?");
+var condition = bool && "carl";
+document.writeln(condition);
+if (condition) {
+    document.writeln("true");schmoo
+}
+else {
+    document.writ;;;;jhyu
+}
+
+var switchStatements = function() {
+    switch (prompt("What is the weather like?")){
+        case "rainy":
+            document.write("Remember to bring an umbrella.");
+            break;
+        case "sunny":
+            document.write("Dress lightly.");
+        case "cloudy":
+            document.write("Go outside.");
+            break;
+        default:
+            document.write("Unknown weather type!");
+            break;
+    }
+}
